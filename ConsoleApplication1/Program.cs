a2d9c0e587a7d6f2a86f215b0198f0b8b4111aa8
jiaokang <Administrator@HP201-1>
2017-05-16 14:55:10 +0800

    done

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -23,13 +23,53 @@ namespace ConsoleApplication1
             #endregion
 
             #region 数组取值
-            int[] array = new int[3] { 1, 2, 3 };//先赋值
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.WriteLine(array[i]);  //循环遍历打印数组里面的值
-            }
+            /*  int[] array = new int[3] { 1, 2, 3 };//先赋值
+              for (int i = 0; i < array.Length; i++)
+              {
+                  Console.WriteLine(array[i]);  //循环遍历打印数组里面的值
+              }*/
             #endregion
+            #region 练习 
+            /*  string[] arr1 = new string[4] { "c语言", "c++", "c#", "java" };
+              string[] arr2 = { "c语言", "c++", "c#", "java" };
+              string[] arr3 = new string[] { "c语言", "c++", "c#", "java" };*/
 
+            /*  int[] arry = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+              int sum = 0;
+              for (int i = 0; i < arry.Length; i++)
+              {
+                  Console.WriteLine(arry[i]);
+                  sum = sum + arry[i];
+              }
+              Console.WriteLine("数组所有元素之和是:{0}",sum);*/
+
+            //输入7名学生成绩,分别求出最高分,最低分,平均分
+            float[] score = new float[7];
+            for (int i = 0; i < score.Length; i++)
+            {
+                score[i] = Convert.ToSingle(Console.ReadLine());
+            }
+            float sum = 0;
+            float avg = 0;
+            float max = score[0];
+            float min = score[0];
+            for (int i = 0; i < score.Length; i++)
+            {
+                sum = sum + score[i];
+                if (max < score[i])
+                {
+                    max = score[i];
+                }
+                if (min > score[i])
+                {
+                    min = score[i];
+                }
+            }
+            avg = sum / score.Length;
+            Console.WriteLine("最高分是:{0},最低分是:{1},平均分是:{2}",max,min,avg);
+
+
+            #endregion
 
         }
     }
